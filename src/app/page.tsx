803562bcc06001ba42d937bbb609ec255c869e05
aryan <agarwalaryan139@gmail.com>
2024-07-11 03:05:17 +0530

    fix: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,27 +9,27 @@ export default function Home() {
     { amount: "69$", link: "https://tiplink.io/t#4JoRiNvTR5Uyn45ST" },
   ];
 
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-950 via-gray-900 to-black text-white">
-      <Navbar />
-      <main className="container mx-auto p-4">
-        <Profile
-          image="https://pbs.twimg.com/profile_images/1716974438464339968/YN2EYZEH_400x400.jpg"
-          name="aryan"
-          username="_0xaryan"
-          bio="dev, i like to build cool stuff"
-          totalTipsSOL={420}
-          totalTipsUSDC={69}
-        />
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <div className="md:col-span-4">
-            <TipsTable tips={tips} />
-          </div>
-        </div>
-      </main>
-    </div>
-  );
   // return (
-  //   <LoginScreen />
-  // )
+  //   <div className="min-h-screen bg-gradient-to-br from-gray-950 via-gray-900 to-black text-white">
+  //     <Navbar />
+  //     <main className="container mx-auto p-4">
+  //       <Profile
+  //         image="https://pbs.twimg.com/profile_images/1716974438464339968/YN2EYZEH_400x400.jpg"
+  //         name="aryan"
+  //         username="_0xaryan"
+  //         bio="dev, i like to build cool stuff"
+  //         totalTipsSOL={420}
+  //         totalTipsUSDC={69}
+  //       />
+  //       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+  //         <div className="md:col-span-4">
+  //           <TipsTable tips={tips} />
+  //         </div>
+  //       </div>
+  //     </main>
+  //   </div>
+  // );
+  return (
+    <LoginScreen />
+  )
 }
